4cc830f741ad9b0318501cd20dd236848ea51992
Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>
2017-03-22 23:59:47 +0100

    Vector is not thread safe
    Signed-off-by: Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>

--- 16.03/1/source.cpp
+++ 16.03/1/source.cpp
@@ -13,20 +13,24 @@ typedef struct s_v3 {
 
 int main (int argc, char *argv[]) {
 
-    int rank, size, lineAmount;
     MPI_Init(&argc, &argv);
+    int rank, size, lineAmount, nodeVecAmount;
+    float* nodeElem;
+    std::vector<Vec3> vectorList;
+
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 	
 	if (rank == 0){
 		std::ifstream myFile;
-        myFile.open("../AR/v01.dat");
+        //myFile.open("../AR/v01.dat");
+        myFile.open("../AR/small_test.dat");
 		lineAmount = std::count(std::istreambuf_iterator<char>(myFile),
 			std::istreambuf_iterator<char>(), '\n');
 		std::cout << lineAmount << "\n";
 		
 		float* allVec = (float*)malloc(sizeof(float) * lineAmount * 3);
-		int allVecIter = 0;
+		int allElemIter = 0;
 		
 		myFile.seekg(0, myFile.beg);
         while(!myFile.eof()) {
@@ -34,42 +38,92 @@ int main (int argc, char *argv[]) {
             myFile.getline(line, 256);
             int c = 0; //helps set current char to curVal
             int vi = 0; //temporary vector counter. It resets itself every line
-            //Vec3 tempV; //temporary vector
             for (int i = 0 ; line[i] != 0 ; i++) { //go through the line while any chars left
                 if (line[i] != 0 && line[i] != 32) { //if there are required characters
                     curVal[c++] = line[i];
                     if (line[i+1] == 0 || line[i+1] == 32) { //if next char in line is a whitespace
-						allVec[allVecIter++] = atof(curVal);
-						/*tempV.x[vi++] = atof(curVal); //save currently readed value to the temporary vector
-                        if (vi == 3)
-                            vectorList.push_back(tempV);
-						*/
+						allVec[allElemIter++] = atof(curVal);
                         c = 0; //reset current char iterator
                         for (int j = 0 ; j < 8 ; j++) //reset current value to be clean for next reading
                             curVal[j] = '\0';
                     }
                 }
             }
-			
         }
 		
-		int* vecForRank = (int*)malloc(sizeof(int) * size);
-		int vecLeft = allVecIter;
-		div_t tempDiv;
+		int* vecForRank = (int*)calloc(size, sizeof(int));
+		int vecLeft = lineAmount; //will use this value to recognise how many vectors are left
+		div_t tempDiv; //for counting left vectors
 		do{
 			tempDiv = div(vecLeft, size); //amount of vectors for each processor
 			for (int i = 0 ; i < size ; i++){
-				vecForRank[i] = tempDiv.quot;
-				std::cout << vecForRank[i] << "\n";
+				vecForRank[i] += tempDiv.quot;
 			}
-			if (tempDiv.rem > 0)
+            vecLeft = tempDiv.rem; //update left vectors
+			if (tempDiv.rem > size)
 				vecLeft = tempDiv.rem;
-				
-		}while (tempDiv.rem > 0);
+			else {
+                int ti = 0; //temporary iterator
+                while (vecLeft > 0){
+                    vecForRank[(ti++) % size]++;
+                    vecLeft--; //add 1 to each node until no vectors left
+                }
+            }
+		}while (vecLeft > 0);
+
+        int stepGate = vecForRank[rank]; //value used for initialising sending start point
+        for (int i = 1 ; i < size ; i++){
+            MPI_Send(&vecForRank[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(allVec+stepGate, vecForRank[i]*3, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
+            stepGate += vecForRank[i];
+        }
+
+        nodeVecAmount = vecForRank[rank];
+        Vec3 tempVec;
+        for (int i = 0 ; i < nodeVecAmount*3 ; i++){
+            tempVec.x[i % 3] = allVec[i];
+            if (i > 0 && ((i % 3) == 0))
+                vectorList.push_back(tempVec);
+        }
 		
         myFile.close();
 		free(allVec);
+        free(vecForRank);
 	}
+
+    if (rank != 0){
+        MPI_Status* status;
+        MPI_Recv(&nodeVecAmount, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, status);
+        std::cout << "node vec amount: " << nodeVecAmount << "\n";
+        nodeElem = (float*)malloc(sizeof(float) * nodeVecAmount*3);
+        MPI_Recv(nodeElem, nodeVecAmount*3, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, status);
+
+        for (int i = 0 ; i < nodeVecAmount * 3 ; i++)
+            std::cout << nodeElem[i] << "\n";
+
+
+
+        Vec3 tempVec;
+        for (int i = 0 ; i < nodeVecAmount*3 ; i++){
+            tempVec.x[i % 3] = nodeElem[i];
+            std::cout << i%3;
+            if (i > 0 && ((i % 3) == 2)) {
+                //vectorList.push_back(tempVec); //vector is not thread-safe
+                std::cout << "i";
+            }
+        }
+
+        std::cout << "vectorList.size(): " << vectorList.size() << "\n";
+//
+//        for (int i = 0 ; i < vectorList.size() ; i++){
+//            std::cout << vectorList[i].x[0] << "\n";
+//        }
+
+
+        free(nodeElem);
+    }
+
+
 
  /*   if (rank == 0) {
         std::vector<Vec3> vectorList;
@@ -99,7 +153,6 @@ int main (int argc, char *argv[]) {
 
     }*/
 
-	
     MPI_Finalize();
 	return 0;
 
