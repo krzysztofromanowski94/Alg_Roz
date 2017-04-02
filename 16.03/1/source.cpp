d3248115d5e3ae31729f3092ebafbed074b5619e
Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>
2017-04-02 11:23:44 +0200

    Add sum timing
    Signed-off-by: Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>

--- 16.03/1/source.cpp
+++ 16.03/1/source.cpp
@@ -6,9 +6,7 @@
 #include <cstdlib>
 #include "mpi.h"
 
-typedef struct s_v3 {
-    float x[3];
-} Vec3;
+
 
 int main (int argc, char *argv[]) {
 
@@ -17,61 +15,64 @@ int main (int argc, char *argv[]) {
     float* nodeElem;
     float average[]= {0,0,0};
     float sum, l;
+    double timeTotal, timeReadData, timeProcessData, timeReduceResults;
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-	
-	if (rank == 0){
-            std::ifstream myFile;
-            myFile.open("../AR/v01.dat");
 
-            lineAmount = std::count(std::istreambuf_iterator<char>(myFile),
-			std::istreambuf_iterator<char>(), '\n');
-            printf("Amount of vectors: %i\n", lineAmount);
+    if (rank == 0){
+        timeReadData = MPI_Wtime(); // start reading time
+
+        std::ifstream myFile;
+        myFile.open("../AR/v01.dat");
+
+        lineAmount = std::count(std::istreambuf_iterator<char>(myFile),
+                                std::istreambuf_iterator<char>(), '\n');
+        printf("Amount of vectors: %i\n", lineAmount);
 
 
-            float* allVec = (float*)malloc(sizeof(float) * lineAmount * 3);
-            int allElemIter = 0;
-		
-            myFile.seekg(0, myFile.beg);
-                while(!myFile.eof()) {
-                char line[256], curVal[] = {'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0'}; //current value
-                myFile.getline(line, 256);
-                int c = 0; //helps set current char to curVal
-                int vi = 0; //temporary vector counter. It resets itself every line
-                for (int i = 0 ; line[i] != 0 ; i++) { //go through the line while any chars left
-                    if (line[i] != 0 && line[i] != 32) { //if there are required characters
-                        curVal[c++] = line[i];
-                        if (line[i+1] == 0 || line[i+1] == 32) { //if next char in line is a whitespace
-                            allVec[allElemIter++] = atof(curVal);
-                            c = 0; //reset current char iterator
-                            for (int j = 0 ; j < 8 ; j++) //reset current value to be clean for next reading
-                                curVal[j] = '\0';
+        float* allVec = (float*)malloc(sizeof(float) * lineAmount * 3);
+        int allElemIter = 0;
+
+        myFile.seekg(0, myFile.beg);
+        while(!myFile.eof()) {
+            char line[256], curVal[] = {'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0'}; //current value
+            myFile.getline(line, 256);
+            int c = 0; //helps set current char to curVal
+            int vi = 0; //temporary vector counter. It resets itself every line
+            for (int i = 0 ; line[i] != 0 ; i++) { //go through the line while any chars left
+                if (line[i] != 0 && line[i] != 32) { //if there are required characters
+                    curVal[c++] = line[i];
+                    if (line[i+1] == 0 || line[i+1] == 32) { //if next char in line is a whitespace
+                        allVec[allElemIter++] = atof(curVal);
+                        c = 0; //reset current char iterator
+                        for (int j = 0 ; j < 8 ; j++) //reset current value to be clean for next reading
+                            curVal[j] = '\0';
                     }
                 }
             }
         }
         myFile.close();
 
-	int* vecForRank = (int*)calloc(size, sizeof(int));
-	int vecLeft = lineAmount; //will use this value to recognise how many vectors are left
-	div_t tempDiv; //for counting left vectors
-	do{
-		tempDiv = div(vecLeft, size); //amount of vectors for each processor
-		for (int i = 0 ; i < size ; i++){
-			vecForRank[i] += tempDiv.quot;
-		}
-		vecLeft = tempDiv.rem; //update left vectors
-		if (tempDiv.rem > size)
-			vecLeft = tempDiv.rem;
-		else {
-	                int ti = 0; //temporary iterator
-                        while (vecLeft > 0){
-                            vecForRank[(ti++) % size]++;
-                            vecLeft--; //add 1 to each node until no vectors left
-                        }
+        int* vecForRank = (int*)calloc(size, sizeof(int));
+        int vecLeft = lineAmount; //will use this value to recognise how many vectors are left
+        div_t tempDiv; //for counting left vectors
+        do{
+            tempDiv = div(vecLeft, size); //amount of vectors for each processor
+            for (int i = 0 ; i < size ; i++){
+                vecForRank[i] += tempDiv.quot;
+            }
+            vecLeft = tempDiv.rem; //update left vectors
+            if (tempDiv.rem > size)
+                vecLeft = tempDiv.rem;
+            else {
+                int ti = 0; //temporary iterator
+                while (vecLeft > 0){
+                    vecForRank[(ti++) % size]++;
+                    vecLeft--; //add 1 to each node until no vectors left
                 }
-	}while (vecLeft > 0);
+            }
+        }while (vecLeft > 0);
 
         int stepGate = vecForRank[rank] * 3; //value used for initialising sending start point
         for (int i = 1 ; i < size ; i++){
@@ -80,8 +81,11 @@ int main (int argc, char *argv[]) {
             stepGate += vecForRank[i] * 3;
         }
 
-	sum = 0;
-	l = 0;
+        timeReadData = MPI_Wtime() - timeReadData; // end of reading and spreading data
+        timeProcessData = MPI_Wtime();
+
+        sum = 0;
+        l = 0;
         int nodeVecAmount = vecForRank[rank];
         for (int i = 0 ; i < nodeVecAmount * 3 ; i++){
             sum += pow(allVec[i], 2);
@@ -92,19 +96,25 @@ int main (int argc, char *argv[]) {
             }
         }
 
-	free(allVec);
+        timeProcessData = MPI_Wtime() - timeProcessData;
+
+        free(allVec);
         free(vecForRank);
-	}
+    }
 
     if (rank != 0){
+        timeReadData = MPI_Wtime();
         MPI_Status status;
         int nodeVecAmount;
         MPI_Recv(&nodeVecAmount, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
         nodeElem = (float*)malloc(sizeof(float) * nodeVecAmount*3);
         MPI_Recv(nodeElem, nodeVecAmount*3, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &status);
+        timeReadData = MPI_Wtime() - timeReadData;
 
-	l = 0;
-	sum = 0;
+
+        timeProcessData = MPI_Wtime();
+        l = 0;
+        sum = 0;
         for (int i = 0 ; i < nodeVecAmount * 3 ; i++){
             sum += pow(nodeElem[i], 2);
             average[i % 3] += nodeElem[i];
@@ -113,33 +123,38 @@ int main (int argc, char *argv[]) {
                 sum = 0;
             }
         }
+        timeProcessData = MPI_Wtime() - timeProcessData;
         free(nodeElem);
     }
-    
-    float worldL = 0; // ovarall lengh from all nodes
+
+    timeReduceResults = MPI_Wtime();
+    float worldL = 0; // overall length from all nodes
     float worldAverage[] = {0,0,0}; // overall x,y,z average
-
     MPI_Reduce(&l, &worldL, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
-
     for (int i = 0 ; i < 3 ; i++){
         MPI_Reduce(&(average[i]) ,&(worldAverage[i]), 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
     }
-
-    
     for (int i = 0 ; i < 3 ; i++){
         worldAverage[i] /= lineAmount;
     }
+    if (rank == 0 ){
+        worldL /= lineAmount;
+    }
+
+    timeReduceResults = MPI_Wtime() - timeReduceResults;
 
     if (rank == 0 ){
         for (int i = 0 ; i < 3 ; i++){
             printf("worldAverage[%i]: %f \n", i, worldAverage[i]);
         }
-	worldL /= lineAmount;
         printf("worldL: %f\n", worldL);
+        //printf("Overall time: %f\n", MPI_Wtime() - myTime);
     }
+
+    printf("rank: %i, readData: %f, processData: %f, reduceResults: %f\n", rank, timeReadData, timeProcessData, timeReduceResults);
 
 
     MPI_Finalize();
-	return 0;
+    return 0;
 
 }
