a8fe830b3131e80e1d6d38b05a7475d541a080ee
Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>
2017-04-04 00:52:40 +0200

    Changed hierarchy
    Signed-off-by: Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>

--- 16.03/1/source.cpp
+++ 16.03/1/source.cpp
@@ -9,26 +9,34 @@ int main (int argc, char *argv[]) {
 
     MPI_Init(&argc, &argv);
     int rank, size, lineAmount, nodeVecAmount;
-    float* nodeElem;
+    float* elements;
     float average[]= {0,0,0};
     float sum, l;
     double timeTotal, timeReadData, timeProcessData, timeReduceResults;
+    int option = -1;
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-
+//
+//    if (rank == 0)
+//    {
+//        printf("How would you like to read the file?\n0.: ifstream\n1.: MPI_File");
+//        option = getchar();
+//        printf("You choose %i\n", option);
+//    }
+//    else
     if (rank == 0){
         timeReadData = MPI_Wtime(); // start reading time
 
         std::ifstream myFile;
-        myFile.open("../AR/v01.dat");
+        myFile.open("../AR/v06.dat");
 
         lineAmount = std::count(std::istreambuf_iterator<char>(myFile),
                                 std::istreambuf_iterator<char>(), '\n');
         printf("Amount of vectors: %i\n", lineAmount);
 
 
-        float* allVec = (float*)malloc(sizeof(float) * lineAmount * 3);
+        elements = (float*)malloc(sizeof(float) * lineAmount * 3);
         int allElemIter = 0;
 
         myFile.seekg(0, myFile.beg);
@@ -41,7 +49,7 @@ int main (int argc, char *argv[]) {
                 if (line[i] != 0 && line[i] != 32) { //if there are required characters
                     curVal[c++] = line[i];
                     if (line[i+1] == 0 || line[i+1] == 32) { //if next char in line is a whitespace
-                        allVec[allElemIter++] = atof(curVal);
+                        elements[allElemIter++] = atof(curVal);
                         c = 0; //reset current char iterator
                         for (int j = 0 ; j < 8 ; j++) //reset current value to be clean for next reading
                             curVal[j] = '\0';
@@ -78,55 +86,41 @@ int main (int argc, char *argv[]) {
         int stepGate = vecForRank[rank] * 3; //value used for initialising sending start point
         for (int i = 1 ; i < size ; i++){
             MPI_Send(&(vecForRank[i]), 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Send(allVec+stepGate, vecForRank[i]*3, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(elements+stepGate, vecForRank[i]*3, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
             stepGate += vecForRank[i] * 3;
         }
 
-        timeReadData = MPI_Wtime() - timeReadData; // end of reading and spreading data
-        timeProcessData = MPI_Wtime();
-
-        sum = 0;
-        l = 0;
-        int nodeVecAmount = vecForRank[rank];
-        for (int i = 0 ; i < nodeVecAmount * 3 ; i++){
-            sum += pow(allVec[i], 2);
-            average[i % 3] += allVec[i];
-            if (i > 0 && i % 3 == 2 || i == nodeVecAmount *3 - 1){
-                l += sqrt(sum);
-                sum = 0;
-            }
-        }
-
-        timeProcessData = MPI_Wtime() - timeProcessData;
-
-        free(allVec);
+        nodeVecAmount = vecForRank[rank];
         free(vecForRank);
+        timeReadData = MPI_Wtime() - timeReadData; // end of reading and spreading data
     }
 
-    if (rank != 0){
+    if (rank != 0) {
         timeReadData = MPI_Wtime();
         MPI_Status status;
-        int nodeVecAmount;
         MPI_Recv(&nodeVecAmount, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-        nodeElem = (float*)malloc(sizeof(float) * nodeVecAmount*3);
-        MPI_Recv(nodeElem, nodeVecAmount*3, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &status);
+        elements = (float *) malloc(sizeof(float) * nodeVecAmount * 3);
+        MPI_Recv(elements, nodeVecAmount * 3, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &status);
         timeReadData = MPI_Wtime() - timeReadData;
-
-
-        timeProcessData = MPI_Wtime();
-        l = 0;
-        sum = 0;
-        for (int i = 0 ; i < nodeVecAmount * 3 ; i++){
-            sum += pow(nodeElem[i], 2);
-            average[i % 3] += nodeElem[i];
-            if (i > 0 && i % 3 == 2 || i == nodeVecAmount * 3 - 1){
-                l += sqrt(sum);
-                sum = 0;
-            }
-        }
-        timeProcessData = MPI_Wtime() - timeProcessData;
-        free(nodeElem);
     }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+    /// Process data
+
+    timeProcessData = MPI_Wtime();
+    l = 0;
+    sum = 0;
+    for (int i = 0 ; i < nodeVecAmount * 3 ; i++){
+        sum += pow(elements[i], 2);
+        average[i % 3] += elements[i];
+        if (i > 0 && i % 3 == 2 || i == nodeVecAmount * 3 - 1){
+            l += sqrt(sum);
+            sum = 0;
+        }
+    }
+    timeProcessData = MPI_Wtime() - timeProcessData;
+
+    free(elements);
 
     timeReduceResults = MPI_Wtime();
     float worldL = 0; // overall length from all nodes
